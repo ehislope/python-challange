12e129e67c0dbf971e895e26dff9715ddeb37592
Erin Hislope <erinhislope@Erins-MacBook-Pro.local>
2020-07-13 17:30:25 -0700

    updating PyPoll files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,10 @@
-# First we'll import the os module
+# import the os module
 # This will allow us to create file paths across operating systems
 import os
 
-# Module for reading CSV files
+# import Module for reading CSV files
 import csv
+
 # import CSV
 csvpath = os.path.join('Resources', 'election_data.csv')
 
@@ -19,8 +20,8 @@ with open (csvpath, newline='') as csvfile:
     # assign value to variables
     votes = 0
     candidate = ["Khan","Correy","Li", "O'Tooley"]
-    kahn_votes = 0
-    kahn_percent = 0
+    khan_votes = 0
+    khan_percent = 0
     correy_votes = 0
     correy_percent = 0
     li_votes = 0
@@ -33,28 +34,36 @@ with open (csvpath, newline='') as csvfile:
             votes = votes + 1
             # count votes per candidate
             candidate.append(str(Row[2]))
+    # count votes and calculate % for each candidate
     khan_votes= candidate.count(candidate[0])
-    khan_percent = kahn_votes/votes
+    khan_percent = (khan_votes/votes)*100
     correy_votes= candidate.count(candidate[1])
-    correy_percent = correy_votes/votes
+    correy_percent = (correy_votes/votes)*100
     li_votes= candidate.count(candidate[2])
-    li_percent = li_votes/votes
+    li_percent = (li_votes/votes)*100
     otooley_votes= candidate.count(candidate[3])
-    otooley_percent = otooley_votes/votes
+    otooley_percent = (otooley_votes/votes)*100
 
-        # A complete list of candidates who received votes
-            # candidate_names = ["Khan", "Correy", "Li", "O'Tooley"]
-            # vote_count_khan= candidate_names.count("Khan") + 1
-
-    
-    
+      
 # The winner of the election based on popular vote
+
+# output analysis to text file
+output_path = os.path.join("Analysis", "Analysis.txt")
+f = open(output_path, 'w')
+print("Election Results", file=f)
+print("-------------------------------------", file=f)
+print(f"Total Votes: {votes}", file=f)
+print("-------------------------------------", file=f)
+print(f'{candidate[0]}: {khan_percent:.3f}, ({khan_votes})', file=f)
+print(f'{candidate[1]}: {correy_percent:.3f}, ({correy_votes})', file=f)
+print(f'{candidate[2]}: {li_percent:.3f}, ({li_votes})', file=f)
+print(f'{candidate[3]}: {otooley_percent:.3f}, ({otooley_votes})', file=f)
 # output in terminal
 print("Election Results")
 print("-------------------------------------")
 print(f"Total Votes: {votes}")
 print("-------------------------------------")
-print(f'{candidate[0]}: {kahn_percent:.3f}, ({kahn_votes})')
+print(f'{candidate[0]}: {khan_percent:.3f}, ({khan_votes})')
 print(f'{candidate[1]}: {correy_percent:.3f}, ({correy_votes})')
 print(f'{candidate[2]}: {li_percent:.3f}, ({li_votes})')
 print(f'{candidate[3]}: {otooley_percent:.3f}, ({otooley_votes})')
